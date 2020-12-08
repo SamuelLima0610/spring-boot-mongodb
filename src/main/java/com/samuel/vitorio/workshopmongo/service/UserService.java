fbaec082be75fa68b35d11e6ff86e84619902954
Samuel Vitorio Lima <34277130+SamuelLima0610@users.noreply.github.com>
2020-12-08 10:34:52 -0300

    Get the user by Id (There is a Exception handler)

--- src/main/java/com/samuel/vitorio/workshopmongo/service/UserService.java
+++ src/main/java/com/samuel/vitorio/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.samuel.vitorio.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.samuel.vitorio.workshopmongo.domain.User;
 import com.samuel.vitorio.workshopmongo.repository.UserRepository;
+import com.samuel.vitorio.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id){
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("objeto não encontrado"));
 	}
 }
